b2a5ba5b92fdf3976ccba10a39e7004f6eb2888f
Ron Smith <ron.smith@thataintworking.com>
2022-03-18 17:02:57 -0400

    Changed code to use register for PWM

--- src/FanSpeedController.cpp
+++ src/FanSpeedController.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 
 #define PWM25 64
-#define PWM50 128
-#define PWM75 175
+#define PWM50 127
+#define PWM75 191
 #define PWM100 255
 
 void setup() {
@@ -11,21 +11,27 @@ void setup() {
   pinMode(PIN_PB2, INPUT_PULLUP);
   pinMode(PIN_PB3, INPUT_PULLUP);
   pinMode(PIN_PB4, OUTPUT);
+
+  bitSet(PLLCSR, PCKE);   // Use 64MHz internal asynchronous clock
+	bitSet(TCCR1,  CS10);   // No prescaler
+  bitSet(GTCCR,  COM1B1); // OC1B (PIN_PB4) to HIGH on $00 and to LOW on match with OCR1B
+  
+	OCR1B = 0;              // Start with 0% duty cycle
 }
 
 void loop() {
   
   if (digitalRead(PIN_PB2) == LOW) {
-    analogWrite(PIN_PB4, PWM25);
+    OCR1B = PWM25;
   }
   else if (digitalRead(PIN_PB1) == LOW) {
-    analogWrite(PIN_PB4, PWM50);
+    OCR1B = PWM50;
   } 
   else if (digitalRead(PIN_PB0) == LOW) {
-    analogWrite(PIN_PB4, PWM75);
+    OCR1B = PWM75;
   } 
   else if (digitalRead(PIN_PB3) == LOW) {
-    analogWrite(PIN_PB4, PWM100);
+    OCR1B = PWM100;
   }
   // else don't change
 }
